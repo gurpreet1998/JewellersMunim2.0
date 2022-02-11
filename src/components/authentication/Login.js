d8c232718255898c8d8c25120a0c68b6561a06eb
Shubham Gupta <shubham.gupta@hanu.com>
2022-02-11 08:37:59 +0000

    Merged PR 123: Fix Configuration Issue
    Related work items: #1257

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -7,6 +7,13 @@ const Login = () => {
     <AuthCardLayout>
       <h4 className="fs-3 fs-md-2 fs-lg-3 text-400">Welcome!</h4>
       <h5 className={'text-600 pb-3'}>Sign in to the Choice Portal</h5>
+      <small>
+        You are running this application in<b>{process.env.NODE_ENV}</b>mode.
+      </small>
+      <br />
+      <small>
+        Application URL :<b>{process.env.REACT_APP_API_URI}</b>
+      </small>
       <LoginForm layout="card" hasLabel />
     </AuthCardLayout>
   );
