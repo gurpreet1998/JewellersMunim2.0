46c552b2eca333b26f32dbef0d658baa351875da
Shubham Gupta <shubham.gupta@hanu.com>
2022-04-13 07:06:55 +0000

    Merged PR 346: environment fixes
    - test env build
    - Merge branch 'main' into users/shubham/test-build
    - add environment visibility
    - environment fixes
    - environment changes
    
    Related work items: #1465

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -8,20 +8,13 @@ const Login = () => {
     <AuthCardLayout>
       <h4 className="fs-3 fs-md-2 fs-lg-3 text-400">Welcome!</h4>
       <h5 className={'text-600 pb-3'}>Sign in to the Choice Portal</h5>
-      {process.env.NODE_ENV === 'development' ? (
-        <>
-          <p className={'fs--2 pb-0'}>
-            You are running this application in <b>{process.env.NODE_ENV}</b>{' '}
-            mode.
-          </p>
-          <p className={'fs--2 pb-0'}>
-            Application API URI: <b>{process.env.REACT_APP_API_URI}</b> <br />
-            Application Web URI: <b>{process.env.REACT_APP_REDIRECT_URI}</b>
-          </p>
-        </>
-      ) : (
-        <></>
-      )}
+      <p className={'fs--2 pb-0'}>
+        You are running this application in <b>{process.env.NODE_ENV}</b> mode.
+      </p>
+      <p className={'fs--2 pb-0'}>
+        Application API URI: <b>{process.env.REACT_APP_API_URI}</b> <br />
+        Application Web URI: <b>{process.env.REACT_APP_REDIRECT_URI}</b>
+      </p>
       <LoginForm layout="card" hasLabel />
     </AuthCardLayout>
   );
